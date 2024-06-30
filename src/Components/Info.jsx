fde59df765e20ae0f639c1bbf8e8557d11d4fb6d
ViceMusic5 <106986143+ViceMusic@users.noreply.github.com>
2024-06-30 22:24:38 +0800

    切了点图

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,5 +1,22 @@
 import './ComCss.css'
+import { Rate,Avatar} from 'antd';
+import { DislikeFilled, DislikeOutlined, LikeFilled, LikeOutlined } from '@ant-design/icons';
+import React, { createElement, useState } from 'react';
+
+ const comments=[
+  {id:'8008008000', name:'张三', comment:'好用的', ratio:3},
+  {id:'8008008000', name:'张三', comment:'好用的', ratio:3},
+  {id:'8008008000', name:'张三', comment:'好用的', ratio:3},
+  {id:'8008008000', name:'张三', comment:'好用的', ratio:3},
+  {id:'8008008000', name:'张三', comment:'好用的', ratio:3},
+  {id:'8008008000', name:'张三', comment:'好用的', ratio:3},
+  {id:'8008008000', name:'张三', comment:'好用的', ratio:3},
+  {id:'8008008000', name:'张三', comment:'好用的', ratio:3},
+]
+
+
 function Info() {
+  // 关于评论区的一些内容
   return (
     <div className='info radium'> 
     {/* 前面是文件夹的对应格式*/}
@@ -20,7 +37,7 @@ function Info() {
 
     {/* 前面是文件夹的对应格式(选中具体的文件的时候*/}
     <h3>前端开发规范.pdf</h3><br/>
-    {/*这里包括一些下载什么的内容*/}
+    {/* 具体的信息以及评分 */}
     <button style={{padding:5}}> 组织文件夹</button><br/><br/>
     <table >
       <tr>
@@ -36,6 +53,35 @@ function Info() {
         <td>2023-4-16</td>
       </tr>
     </table>  
+    <br></br>
+    <Rate disabled defaultValue={4} />
+    {/*评论内容*/}
+    <div style={{
+      backgroundColor:'rgb(229, 229, 229)',
+      height:'35%',
+      marginTop:20,
+      border:'none',
+      borderRadius:8,
+      padding:10,
+      overflowX:'none',
+      overflowY:'auto',
+
+    }}>
+      {comments.map(item => 
+        <div style={{backgroundColor:'white', 
+        padding:10,
+        border:'none',
+        borderRadius:8,
+        fontSize:15,
+        marginBottom:15
+
+        }}>
+          评论信息
+        </div>
+      )}
+    </div>
+    {/*评论区*/}
+    <div></div>
     </div>
 
   );
